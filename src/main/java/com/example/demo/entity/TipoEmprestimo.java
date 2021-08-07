45b15d8aaccd1eabb45a36516d3191073c333e44
adielmo <adielmorabelo@gmail.com>
2021-08-07 16:11:45 -0300

    Fazendo correções

--- src/main/java/com/example/demo/entity/TipoEmprestimo.java
+++ src/main/java/com/example/demo/entity/TipoEmprestimo.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
 public enum TipoEmprestimo {
-	Pessoal
+	Pessoal,
+	com_Garantia
 }
--- src/main/java/com/example/demo/service/EmprestimoService.java
+++ src/main/java/com/example/demo/service/EmprestimoService.java
@@ -22,13 +22,31 @@ public class EmprestimoService {
 		if (temSalarioLimeteMoraSp(cliente)) {
 			return salarioAteMoraSp(cliente);
 		}
-
+		
+		
+		 if (temSalarioFaixaLimeteMoraSp(cliente)) {
+		  return salarioFaixaSp(cliente);
+		 }
+		
 		throw new IllegalArgumentException("Error !!");
 	}
 
-	private ModalidadeEmprestimoResponse salarioAteMoraSp(NewClienteRequest cliente) {
-		return new ModalidadeEmprestimoResponse(cliente, Set.of(new PossibilidadeEmprestimoResponse(TipoEmprestimo.Pessoal, 3)));
+	
+	private ModalidadeEmprestimoResponse salarioFaixaSp(NewClienteRequest cliente) {
+		return new ModalidadeEmprestimoResponse(cliente,
+				  Set.of(new PossibilidadeEmprestimoResponse(TipoEmprestimo.Pessoal, 4),
+				         new PossibilidadeEmprestimoResponse(TipoEmprestimo.com_Garantia, 3)));
 	}
+
+
+	private boolean temSalarioFaixaLimeteMoraSp(NewClienteRequest cliente) {
+
+		return cliente.getSalario().compareTo(new BigDecimal("3000")) > 0
+			&& cliente.getSalario().compareTo(new BigDecimal("5000")) <= 0
+			&& cliente.getEstado().equals("SP")
+			&& cliente.getIdade() < 30;
+	}
+
 
 	private boolean temSalarioLimeteMoraSp(NewClienteRequest cliente) {
 		return cliente.getSalario().compareTo(new BigDecimal("3000")) <= 0 
@@ -46,5 +64,12 @@ public class EmprestimoService {
 		return new ModalidadeEmprestimoResponse(cliente,
 				Set.of(new PossibilidadeEmprestimoResponse(TipoEmprestimo.Pessoal, 4)));
 	}
+	
+	private ModalidadeEmprestimoResponse salarioAteMoraSp(NewClienteRequest cliente) {
+		return new ModalidadeEmprestimoResponse(cliente, 
+				Set.of(new PossibilidadeEmprestimoResponse(TipoEmprestimo.Pessoal, 3),
+						new PossibilidadeEmprestimoResponse(TipoEmprestimo.com_Garantia, 4)));
+	}
+
 
 }
